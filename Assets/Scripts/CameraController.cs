41a2949f04d5f0f5f38fae0988a0b9003e1fd240
brigettea1234 <brigettea1234@gmail.com>
2023-08-03 21:50:50 +1000

    FixCameraScript

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public enum CameraStyle { Fixed,Free}
+public enum CameraStyle {Fixed, Free}
 public class CameraController : MonoBehaviour
 {
     public GameObject player;
